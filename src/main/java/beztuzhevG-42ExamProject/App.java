beea18f4d44cfced66e43dc2ba82ea02a136cf5c
dbeztuzhev <epapirua@gmail.com>
2019-11-17 12:21:31 +0200

    first commit

---
+++ src/main/java/beztuzhevG-42ExamProject/App.java
@@ -0,0 +1,13 @@
+package beztuzhevG-42ExamProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
